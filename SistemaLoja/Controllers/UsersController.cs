d61877197841296a4d5262f602ce1adf327e555c
DESKTOP-AEM2SP0\rgril <r.grillo@hotmail.com>
2018-01-17 20:21:46 +0000

    Roles index create

--- SistemaLoja/Controllers/UsersController.cs
+++ SistemaLoja/Controllers/UsersController.cs
@@ -36,7 +36,28 @@ namespace SistemaLoja.Controllers
             return View(usersView);
         }
 
-        public ActionResult Roles(string userId)
+        public ActionResult AddRole(string userId)
+        {
+            //list user already registered on db
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            //list user already registered on db
+            var users = userManager.Users.ToList();
+            var user = users.Find(u => u.Id == userId);
+
+            var userView = new UserView
+            {
+                Email = user.Email,
+                Nome = user.UserName,
+                UserId = user.Id
+
+            };
+
+            return View(userView);
+            return View();
+        }
+
+
+            public ActionResult Roles(string userId)
         {
             //list user already registered on db
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
@@ -44,12 +65,13 @@ namespace SistemaLoja.Controllers
             var users = userManager.Users.ToList();
 
 
+
+            var user = users.Find(u => u.Id == userId);
+
             //access to users roles
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
             var roles = roleManager.Roles.ToList();
             var rolesView = new List<RoleView>();
-
-            var user = users.Find(u => u.Id == userId);
 
             foreach (var item in user.Roles)
             {
