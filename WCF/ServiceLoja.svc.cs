d9e3ddc370cbc005de6a0093c735c67cb9d9af0c
DESKTOP-AEM2SP0\rgril <r.grillo@hotmail.com>
2018-02-01 20:35:47 +0000

    Using WCF to consume services

---
+++ WCF/ServiceLoja.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceLoja : IServiceLoja
+    {
+        public Produtoes GetProduto(int produtoId)
+        {
+            var db = new LojaSistemaEntities();
+            var produto = db.Produtoes.Find(produtoId);
+            db.Dispose();
+            return produto;
+        }
+
+        public List<Produtoes> GetProdutos()
+        {
+            var db = new LojaSistemaEntities();
+            var produtos = db.Produtoes.ToList();
+            db.Dispose();
+            return produtos;
+        }
+    }
+}
